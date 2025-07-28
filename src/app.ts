632b929a870a7bad8435b7119473b70af6268b14
MatheusFigueiredo1001 <figueiredom924@gmail.com>
2025-07-28 10:13:43 -0300

    Adicionando CORS

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ import Fastify from 'fastify';
 import type { FastifyInstance } from 'fastify';
 import { env } from './config/env';
 import routes from './routes';
+import cors from '@fastify/cors';
 
 const app: FastifyInstance = Fastify({
   logger: {
     level: env.NODE_ENV === 'dev' ? 'info' : 'error',
   },
 });
+
+app.register(cors);
 
 app.register(routes, { prefix: '/api' });
 
