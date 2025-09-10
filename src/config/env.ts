3116c7fb0ad04c71510b5eaf4ee63a7c1015a881
MatheusFigueiredo1001 <figueiredom924@gmail.com>
2025-09-10 14:46:53 -0300

    Ajustes nas variáveis de ambiente

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,7 +4,7 @@ import { z } from 'zod';
 dotenv.config();
 
 const envSchema = z.object({
-  PORT: z.string().transform(Number).default('3001'),
+  PORT: z.string().transform(Number).default('3003'),
   DATABASE_URL: z.string().min(8, 'DATABASE_URL it has to be mandatory!'),
   NODE_ENV: z.enum(['dev', 'test', 'prod'], {
     message: 'NODE_ENV it must be dev, test or prod!',
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -17,7 +17,7 @@ const initializeFirebaseAdmin = (): void => {
     admin.initializeApp({
       credential: admin.credential.cert({
         projectId: FIREBASE_PROJECT_ID,
-        privateKey: FIREBASE_PRIVATE_KEY,
+        privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n').trim(),
         clientEmail: FIREBASE_CLIENT_EMAIL,
       }),
     });
