453eeb78c395fad9f4a9ab8532a3a62c5c37e82b
MatheusFigueiredo1001 <figueiredom924@gmail.com>
2025-09-10 14:56:46 -0300

    Configuração para deploy

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ const startServer = async () => {
 
     await initializeGlobalCategories();
 
-    await app.listen({ port: PORT }).then(() => {
+    await app.listen({ port: PORT, host: '0.0.0.0' }).then(() => {
       console.log(`Server is running at port ${PORT}`);
     });
   } catch (err) {
